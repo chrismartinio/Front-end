66a14a5d1e99740e7ca21e96d67127fd7727d731
Julie <julie@julies-air.attlocal.net>
2019-02-28 13:08:21 -0800

    Drew changed signuppage.js

--- screens/SignUpFlow/SignupPage.js
+++ screens/SignUpFlow/SignupPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import {
   Image,
   Platform,
@@ -9,13 +9,12 @@ import {
   View,
   Button
 } from 'react-native';
-import { WebBrowser } from 'expo';
 import { LinearGradient } from 'expo';
+// import Categories from '../../components/SignUpFlow/Categories'
+const Form = t.form.Form;
 import t from 'tcomb-form-native';
 import { connect } from 'react-redux'
 import SetProfilePersonalAction from '../../storage/actions/SetProfilePersonalAction'
-
-const Form = t.form.Form;
 
 var Positive = t.refinement(t.Number, function (n) {
   return n >= 18;
@@ -33,11 +32,12 @@ var details = t.struct({
   age: Positive, // refinement
   gender: Gender,
 });
-
 class SignupPage extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+  // static navigationOptions = {
+  //   header: null,
+  // };
+
+  //having null header means no back  button is present!
 
   handleSubmit = () => {
     const value = this._form.getValue();
@@ -45,172 +45,47 @@ class SignupPage extends React.Component {
     this.props.navigation.navigate('Registration');
   }
 
+  render(){
+    return(
+      <View  style={{flex:1}}>
 
-  render() {
-    return (
-      <View style={styles.container}>
-
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <LinearGradient
-            colors={['#4c669f', '#3b5998', '#192f6a']}
-            style={{ alignItems: 'center'}}>
-
+        <LinearGradient
+          colors={['#18cdf6', '#43218c']}
+          style={{flex:1}}
+        >
 
 
+        <ScrollView>
 
-          <View style={styles.formContainer}>
-                <Form
+          <Text style={{top:70}}>
+            This is the sign-up page
+          </Text>
+
+          <View style={{top:100}}>
+          <Form
                   type={details}
                   ref={d => this._form = d}
                 />
-              </View>
+          </View>
 
-
-          <View>
+          <View style={{top:250,width:'50%', backgroundColor:'white', right:'-25%'}}>
             <Button
-              title="Select Your Activities!"
               onPress={this.handleSubmit}
+              title='Continue'
               color='blue'
             />
           </View>
 
 
-
-          </LinearGradient>
         </ScrollView>
 
+
+        </LinearGradient>
       </View>
-    );
+      )
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  formContainer: {
-    justifyContent: 'center',
-    marginTop: 50,
-    padding: 10,
-    backgroundColor: '#ffffff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  welcomeImage: {
-    width: 400,
-    height: 250,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
 
 const mapStateToProps = (state) => ({
   ...state
@@ -220,3 +95,4 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
+
