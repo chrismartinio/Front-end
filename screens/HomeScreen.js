0b1eed8787d85e2d44bfa514f12dc205d1d6cb3c
Daryl Lin <daryllin5@gmail.com>
2019-03-25 15:41:42 -0700

    Fixed type error bug

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -41,7 +41,7 @@ export default class HomeScreen extends React.Component {
     console.log('value: ', value);
     // axios request here:
 
-    const userName = value.split('@')[0]
+
     // use action to send data to store.
 
 
