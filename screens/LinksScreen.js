0fd266992a7f91b63b582d365c46dcc077b87125
Daryl Lin <daryllin5@gmail.com>
2019-04-03 15:53:36 -0700

    Added testig screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,18 +1,80 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import ProfilePage from './SignUpFlow/ProfilePage'
+import RegistrationPage from './SignUpFlow/RegistrationPage'
+import SelfiePage from './SignUpFlow/SelfiePage'
+import SignupPage from './SignUpFlow/SignupPage'
+import t from 'tcomb-form-native';
+const Form = t.form.Form;
+
+
+var Component = t.enums({
+  sPROFILE: 'SignupFlow/ProfilePage',
+  sREGISTRATION: 'SignupFlow/RegistrationPage',
+  sSELFIE: 'SignupFlow/SelfiePage',
+  sSIGNUP: 'SignupFlow/SignupPage'
+},'Component');
+
+var details = t.struct({
+  Component: Component
+});
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Testing Screen',
   };
+  constructor(props){
+    super(props)
+    this.state = {
+      CurrentScreen: ProfilePage
+    }
+  }
+
+  handleChange = () => {
+
+    if(this._form.getValue().Component === 'sPROFILE'){
+      this.setState({
+        CurrentScreen: ProfilePage
+      })
+    } else if(this._form.getValue().Component === 'sREGISTRATION'){
+      this.setState({
+        CurrentScreen: RegistrationPage
+      })
+    }  else if(this._form.getValue().Component === 'sSELFIE'){
+      this.setState({
+        CurrentScreen: SelfiePage
+      })
+    } else if(this._form.getValue().Component === 'sSIGNUP'){
+      this.setState({
+        CurrentScreen: SignupPage
+      })
+    } else {
+      this.setState({
+        CurrentScreen: ProfilePage
+      })
+    }
+  }
+
+  // order
+  // drop dowwn menu
+  // rendered component
 
   render() {
+    let CurrentScreen = this.state.CurrentScreen
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <Form
+            style={{color:'black'}}
+            type={details}
+            ref={d => this._form = d}
+            onChange={this.handleChange}
+        />
+
+        <View>
+        <CurrentScreen />
+        </View>
+
       </ScrollView>
     );
   }
@@ -21,7 +83,6 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
   },
 });
