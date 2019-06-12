01ea6f892c8df7cfc215229c27fc208c5e983319
Daryl Lin <daryllin5@gmail.com>
2019-06-12 16:06:57 -0700

    Testing screens

---
+++ screens/SignUpFlow/SpendAWeekend.js
@@ -0,0 +1,21 @@
+import react from 'react';
+import {
+    View, Text
+} from 'react-native';
+import { LinearGradient } from 'expo';
+
+class SpendAWeekend extends react.Component {
+
+  render(){
+    return(
+        <LinearGradient
+          textStyle={{ color: '#fff' }} colors={['#18cdf6', '#43218c']}
+          style={{flex:1}}
+        >
+
+        </LinearGradient>
+      )
+  }
+}
+
+export default SpendAWeekend;
