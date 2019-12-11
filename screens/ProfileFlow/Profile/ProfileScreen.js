5bfe6f7cc143a10d825c8776042cbd56482daf4c
user155231 <user155231@74-80-250-210.ic2net.net>
2019-12-11 14:53:30 -0800

    adjust profile

--- screens/ProfileFlow/Profile/ProfileScreen.js
+++ screens/ProfileFlow/Profile/ProfileScreen.js
@@ -21,6 +21,10 @@ import Footer from "../../../sharedComponents/Footer";
 
 import { localhost } from "../../../config/ipconfig";
 
+import { Icon } from "react-native-elements";
+
+const { height, width } = Dimensions.get("window");
+
 function _calculateAge(birthday) {
   birthday = new Date(birthday);
   // birthday is a date
@@ -72,7 +76,8 @@ class ProfileScreen extends React.Component {
       age: "",
       city: "",
       state: "",
-      userImage: "https://facebook.github.io/react-native/img/tiny_logo.png",
+      userImage:
+        "https://media.gq.com/photos/56d4902a9acdcf20275ef34c/master/w_806,h_1173,c_limit/tom-hardy-lead-840.jpg",
       likesArray: [],
       userBio: "",
       zipCode: "",
@@ -300,8 +305,10 @@ class ProfileScreen extends React.Component {
       .catch(async err => {
         //console.log(err);
         //HANDLE ANY CATCHED ERRORS
+        //AND WILL TRY TO GET DATA FROM LOCALSTORAGE
 
         //LocalStorage
+        //Check if localstorage guid === current device's user guid
         if (this.guid === this.props.CreateProfileDataReducer.guid) {
           //Get Device's User Data from localStorage device_user_aboutYou table
           let aboutYouObject = await selectDataFromLocalStorage(
@@ -436,7 +443,12 @@ class ProfileScreen extends React.Component {
             source={{
               uri: e
             }}
-            style={{ width: 100, height: 75, borderRadius: 15, margin: 3 }}
+            style={{
+              width: width * 0.267,
+              height: width * 0.2,
+              borderRadius: 15,
+              margin: 3
+            }}
           />
         </TouchableOpacity>
       );
@@ -452,7 +464,7 @@ class ProfileScreen extends React.Component {
                 source={{
                   uri: this.state.userImage
                 }}
-                style={{ width: 350, height: 350, borderRadius: 50 }}
+                style={{ width: width * 0.93, height: width * 0.93, borderRadius: 15 }}
               />
             </View>
 
@@ -463,29 +475,41 @@ class ProfileScreen extends React.Component {
               </Text>
               <Text />
 
-              {/*Map*/}
-              <Button
-                title={"Map"}
-                color={"black"}
-                onPress={() => {
-                  this.props.navigation.navigate("ProfileLocation", {
-                    addressLatitude: this.state.addressLatitude,
-                    addressLongitude: this.state.addressLongitude
-                  });
-                }}
-              />
+              <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+                {/*Map*/}
+                <TouchableOpacity
+                  onPress={() => {
+                    this.props.navigation.navigate("ProfileLocation", {
+                      addressLatitude: this.state.addressLatitude,
+                      addressLongitude: this.state.addressLongitude
+                    });
+                  }}
+                >
+                  <Icon
+                    type="font-awesome"
+                    name="map-marker"
+                    size={width * 0.04}
+                    color="rgb(67, 33, 140)"
+                    iconStyle={{ bottom: 0 }}
+                  />
+                </TouchableOpacity>
 
-              {/*Address*/}
-              <Text style={{ fontSize: 15, fontWeight: "400" }}>
-                {this.state.city}, {this.state.state}
-              </Text>
+                <View style={{ padding: "1%" }} />
+
+                {/*Address*/}
+                <Text style={{ fontSize: 15, fontWeight: "400" }}>
+                  {this.state.city}, {this.state.state}
+                </Text>
+              </View>
             </View>
 
             {/**border line */}
             <View
               style={{
                 borderWidth: 1,
-                borderColor: "#d6d7da"
+                borderColor: "#d6d7da",
+                marginLeft: "5%",
+                marginRight: "5%"
               }}
             />
 
@@ -500,8 +524,11 @@ class ProfileScreen extends React.Component {
             >
               <Text style={{ fontSize: 17, fontWeight: "500" }}>Interests</Text>
             </View>
+            {/*Likes*/}
             <View>
-              <View style={{ flexDirection: "row" }}>{displaylikesArray}</View>
+              <View style={{ flexDirection: "row", margin: "3%" }}>
+                {displaylikesArray}
+              </View>
             </View>
             <View style={{ padding: 7.5 }} />
 
@@ -509,7 +536,9 @@ class ProfileScreen extends React.Component {
             <View
               style={{
                 borderWidth: 1,
-                borderColor: "#d6d7da"
+                borderColor: "#d6d7da",
+                marginLeft: "5%",
+                marginRight: "5%"
               }}
             />
 
@@ -524,6 +553,7 @@ class ProfileScreen extends React.Component {
             >
               <Text style={{ fontSize: 17, fontWeight: "500" }}>About Me</Text>
             </View>
+            {/*Bio*/}
             <View style={{ margin: 15 }}>
               <Text
                 style={{
@@ -541,7 +571,9 @@ class ProfileScreen extends React.Component {
             <View
               style={{
                 borderWidth: 1,
-                borderColor: "#d6d7da"
+                borderColor: "#d6d7da",
+                marginLeft: "5%",
+                marginRight: "5%"
               }}
             />
 
@@ -558,7 +590,8 @@ class ProfileScreen extends React.Component {
             </View>
             <View
               style={{
-                margin: 20
+                margin: 20,
+                alignItems: "center"
               }}
             >
               <View
@@ -587,8 +620,6 @@ class ProfileScreen extends React.Component {
     return this.state.isSuccess ? this.successScreen() : this.loadingScreen();
   }
 }
-
-const { height, width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
