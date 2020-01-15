6e5ae9940ac3b9e0d6d0b4379d09dcbe5a1b17a3
Kevin Nichols <48831113+KRNichols@users.noreply.github.com>
2020-01-15 18:51:27 -0500

    Update ipconfig.js
    updating IP's

--- config/ipconfig.js
+++ config/ipconfig.js
@@ -1,6 +1,6 @@
-export const localhost = '76.106.248.100';
-export const miniServer = '76.106.248.100';
-export const miniServerProd = 'https://nodetests.com';
+export const localhost = '3.225.209.229';
+export const miniServer = '3.225.209.229';
+export const miniServerProd = 'https://devbackend.blindlydate.com';
 export const awsProdServer = 'https://backend.blindlydate.com';
 export const awsDevServer = 'https://devbackend.blindlydate.com';
 //my mac 74.80.250.210
